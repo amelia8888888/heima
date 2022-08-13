2c88c52eb7aacc6c4c836339849c5b332d34b11c
Amelia Tang <940414054@qq.com>
2022-08-13 15:18:33 +0800

    heima

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
+<div class="box"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      show: false
+    }
+  },
+
+  methods: {
+    showPopup () {
+      this.show = true
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.box{
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+}
+</style>
